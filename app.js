ea5aacdb7993b00e94bc09bf10a3de56ff1928e6
Jack <jack.m.dowling26@gmail.com>
2022-02-18 13:56:47 -0500

    initial jquery setup

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+  const $app = $("#app");
+  $app.html("");
+
+  const $title = $('<h2>Discussion</h2>');
+  $title.appendTo($app);
+
+  const $discussion = $('<div id=discussion>Discussion</div>')
+});
