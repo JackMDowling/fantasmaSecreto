ce755acc0dd7f8b01d3b4f922604d7647c5e3bd6
Jack <jack.m.dowling26@gmail.com>
2022-02-23 13:44:40 -0500

    data flowing like the mighty mississippi

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 $(document).ready(() => {
+  const feed = $.ajax({
+    url: "http://localhost:3000/feed/",
+    crossDomain: true,
+    success: function (response) {
+      console.log("hello ", response);
+    },
+    error: function (err) {
+      console.log("this didn't work ", err);
+    },
+  });
+
   const $app = $("#app");
   $app.html("");
 
   const $title = $("<h2>Chat App</h2>");
   $title.appendTo($app);
 
+  const $post = $("<div id=post></div>");
+  $post.appendTo($app);
+
   const $discussion = $("<div id=discussion>Discussion</div>");
   $discussion.appendTo($app);
   const $message = $("<div id=message>Message will go here</div>");
   $message.appendTo($discussion);
+  const $user = $("<div class=user></div>");
+  $user.appendTo($message);
+  const $text = $("<div id=messageText></div>");
+  $text.appendTo($message);
+  const $upvote = $("<div class=upvote></div>");
+  $upvote.appendTo($message);
+  const $reply = $("<div class=reply></div>");
+  $reply.appendTo($post);
 });
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const db = require('./database')
+const db = require("./database");
+
+app.get("/feed/", (req, res) => {
+  db.dbConnection.query("SELECT * FROM messages", (err, data) => {
+    if (err) throw err;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.send(data);
+    res.end();
+  });
+});
 
 app.listen(port, () => {
   console.log(`server connected to port: ${port}`);
