d4096450c4c50d75f1e548c717307f262a08dbd8
Jack <jack.m.dowling26@gmail.com>
2022-02-23 17:07:27 -0500

    Post route written, should be decently sanitised to prevent injection from little Bobby DROP TABLES

--- backend/server.js
+++ backend/server.js
@@ -12,18 +12,29 @@ app.use(
 app.use(bodyParser.json());
 
 app.get("/feed/", (req, res) => {
-  db.dbConnection.query("SELECT * FROM messages", (err, data) => {
-    if (err) throw err;
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.send(data);
-    res.end();
-  });
+  db.dbConnection.query(
+    "SELECT * FROM messages ORDER  BY id DESC LIMIT 5",
+    (err, data) => {
+      if (err) throw err;
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.send(data);
+      res.end();
+    }
+  );
 });
 
 app.post("/submit/", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   console.log("post made it so far");
   console.log(req.body);
+  let { username, text, img } = req.body;
+  db.dbConnection.query(
+    "INSERT INTO messages (username, text, img) VALUES (?, ?, ?);",
+    [username, text, img],
+    (err, data) => {
+      if (err) throw err;
+    }
+  );
 });
 
 app.listen(port, () => {
